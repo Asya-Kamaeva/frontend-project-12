e3deebca597a66c26797d6cad19ae198b609c90a
Asya Zaporozhets <as.kamaeva@yandex.ru>
2022-09-01 21:52:11 +0200

    step2

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => (
               }}
               validationSchema={LoginSchema}
               onSubmit={(values, { resetForm }) => {
-                alert(JSON.stringify(values, null, 2));
+                console.log(JSON.stringify(values, null, 2));
                 resetForm();
               }}
             >
